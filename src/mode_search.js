7b3b3e5f50a282809ebad9299a5029f929f10020
AluaAtykhanova <30786@iitu.edu.kz>
2023-07-11 16:03:01 +0600

    search_mode answered in russia

--- src/mode_search.js
+++ src/mode_search.js
@@ -32,7 +32,7 @@ export async function SearchAndSummurize(ctx, messageText) {
       verbose: true,
     });
     console.log("Loaded agent.");
-    const input = ctx.message.text;
+    const input = "Ответь на русском: " + ctx.message.text;
 
     console.log(`Executing with input "${input}"...`);
 
